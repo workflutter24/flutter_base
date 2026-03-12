0226c36c831e1b84c65210c3312898340e462cac
WorkFlutter24 <work.flutter24@gmail.com>
2026-03-12 12:16:15 +0530

    Create entry point for GetX app in lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'GetX App',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Welcome to GetX!'),
+      ),
+    );
+  }
+}

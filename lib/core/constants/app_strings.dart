a3e34ae999afa8059e23afce0eff9c0d4f21adf8
WorkFlutter24 <work.flutter24@gmail.com>
2026-03-12 12:24:56 +0530

    Create app_strings.dart file with localized strings

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,10 @@
+// app_strings.dart
+
+class AppStrings {
+  static const String hello = 'Hello';
+  static const String welcome = 'Welcome to Flutter';
+  static const String goodbye = 'Goodbye';
+  static const String error = 'An error has occurred';
+  static const String loading = 'Loading...';
+  static const String retry = 'Retry';
+}

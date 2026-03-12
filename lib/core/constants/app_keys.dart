05ef83b01101eed7f769eabcaaff483adf369888
WorkFlutter24 <work.flutter24@gmail.com>
2026-03-12 12:25:17 +0530

    Create app_keys.dart file with local storage keys

---
+++ lib/core/constants/app_keys.dart
@@ -0,0 +1,8 @@
+// Local storage keys for WorkFlutter24 Flutter Base application
+
+class AppKeys {
+  static const String userToken = 'user_token';
+  static const String refreshToken = 'refresh_token';
+  static const String userSettings = 'user_settings';
+  // Add more keys as needed
+}

329ec8fe6a4a6d0082dc00219b07b66e38e02c59
WorkFlutter24 <work.flutter24@gmail.com>
2026-03-12 12:24:46 +0530

    Create api_constants.dart with API endpoints

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,9 @@
+class ApiConstants {
+  static const String baseUrl = "https://api.example.com";
+
+  static const String getUsers = "/users";
+  static const String getPosts = "/posts";
+  static const String getComments = "/comments";
+  static const String getAlbums = "/albums";
+  static const String getPhotos = "/photos";
+}

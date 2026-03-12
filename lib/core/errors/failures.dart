95bca41b30f49bb8a64f50b026ad50f392156f19
WorkFlutter24 <work.flutter24@gmail.com>
2026-03-12 12:25:38 +0530

    Add failures.dart for error handling.

---
+++ lib/core/errors/failures.dart
@@ -0,0 +1,21 @@
+// failures.dart
+
+class Failure {
+  final String message;
+
+  Failure(this.message);
+}
+
+class NetworkFailure extends Failure {
+  NetworkFailure(String message) : super(message);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(String message) : super(message);
+}
+
+class CacheFailure extends Failure {
+  CacheFailure(String message) : super(message);
+}
+
+// Add other failure types as needed

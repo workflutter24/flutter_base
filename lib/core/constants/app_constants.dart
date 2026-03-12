3c359935a89d61275b3cf76ba07ea1eee31207ea
WorkFlutter24 <work.flutter24@gmail.com>
2026-03-12 12:17:01 +0530

    Create app_constants.dart with application constants.

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,7 @@
+// App constants for the application
+
+class AppConstants {
+  static const String appName = 'Flutter Base';
+  static const String version = '1.0.0';
+  // Add more constants here
+}

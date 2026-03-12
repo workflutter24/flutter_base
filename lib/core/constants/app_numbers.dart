291f0ae00d3159e6d8442441a1a695b0f3e07a9c
WorkFlutter24 <work.flutter24@gmail.com>
2026-03-12 12:25:08 +0530

    Create app_numbers.dart with magic numbers

---
+++ lib/core/constants/app_numbers.dart
@@ -0,0 +1,7 @@
+// Magic numbers for the application
+
+const int someMagicNumber = 42;
+const double anotherMagicNumber = 3.14;
+const String appVersion = '1.0.0';
+
+// Add more magic numbers as needed.
